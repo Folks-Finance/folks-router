edb736ce06debbaa0c210be8601d7df04eeaffb6
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2023-09-29 16:20:19 +0200

    feat: add tabs for timeframe, remove useless subtitle

--- apps/website/src/components/chart-cards/histogram-chart-card.tsx
+++ apps/website/src/components/chart-cards/histogram-chart-card.tsx
@@ -1,20 +1,43 @@
-import { Card, BarChart } from "@tremor/react";
+import { Card, BarChart, TabGroup, TabList, Tab } from "@tremor/react";
+
+import { timeframes } from "@components/chart-cards/types";
 
 import type { BarChartProps } from "@tremor/react";
+import type { Dispatch, SetStateAction } from "react";
 
 interface HistogramChartProps extends BarChartProps {
-  title?: string;
-  subtitle?: string;
+  title: string;
+  selectedTimeframeIndex: number;
+  setSelectedTimeframeIndex: Dispatch<SetStateAction<number>>;
 }
 
-export const HistogramChartCard = ({ title, subtitle, ...props }: HistogramChartProps) => (
+export const HistogramChartCard = ({
+  title,
+  selectedTimeframeIndex,
+  setSelectedTimeframeIndex,
+  ...props
+}: HistogramChartProps) => (
   <Card className="flex flex-col gap-y-7 p-7">
-    {(title || subtitle) && (
-      <div className="flex flex-col gap-y-2">
-        {title && <h2 className="text-xl font-semibold text-base-content">{title}</h2>}
-        {subtitle && <p className="text-light-1">{subtitle}</p>}
-      </div>
-    )}
+    <div className="flex items-center">
+      <h2 className="w-1/2 text-2xl text-base-content">{title}</h2>
+      <TabGroup
+        index={selectedTimeframeIndex}
+        onIndexChange={setSelectedTimeframeIndex}
+        className="flex w-1/2 justify-end"
+      >
+        <TabList variant="solid" className="!rounded-md p-1.5">
+          {timeframes.map((timeframe) => (
+            <Tab
+              key={Math.random()}
+              className="rounded-md transition-colors duration-300 hover:bg-base-2 aria-selected:bg-base-2 aria-selected:text-base-content"
+            >
+              {timeframe}
+            </Tab>
+          ))}
+        </TabList>
+      </TabGroup>
+    </div>
+
     <BarChart {...props} />
   </Card>
 );
