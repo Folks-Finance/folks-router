7608c499ced4da9d840c91c91121880856e60994
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2023-09-30 01:35:00 +0200

    feat: retrieve folks router volumes logic

---
+++ apps/website/src/lib/metrics/hooks/use-volumes.ts
@@ -0,0 +1,53 @@
+import { useQuery } from "@tanstack/react-query";
+import { GraphQLClient, gql } from "graphql-request";
+
+interface UseVolumeParams {
+  timeframe: string;
+}
+
+interface BaseVolumeInfo {
+  keys: string[];
+  sum: {
+    volumeUSD: number;
+  };
+}
+
+interface VolumeInfo {
+  routeTransactions: {
+    groupedAggregates: BaseVolumeInfo[];
+  };
+}
+
+const FOLKS_ROUTER_ANALYTICS_ENDPOINT = "https://api.folksrouter.io/analytics/v1/playground";
+
+const getVolume = async ({ timeframe }: UseVolumeParams) => {
+  const client = new GraphQLClient(FOLKS_ROUTER_ANALYTICS_ENDPOINT);
+
+  const query = gql`
+    query RouteTransactions($groupBy: RouteTransactionsGroupBy!) {
+      routeTransactions {
+        groupedAggregates(groupBy: [$groupBy]) {
+          keys
+          sum {
+            volumeUSD
+          }
+        }
+      }
+    }
+  `;
+
+  const { routeTransactions } = await client.request<VolumeInfo>(query, { groupBy: timeframe });
+
+  return { routeTransactions };
+};
+
+export const useVolume = ({ timeframe }: UseVolumeParams) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["volumes", timeframe],
+    queryFn: () => getVolume({ timeframe }),
+    staleTime: Infinity,
+    refetchInterval: 30 * 60 * 1000, // 30 minutes
+  });
+
+  return { data, isLoading };
+};
