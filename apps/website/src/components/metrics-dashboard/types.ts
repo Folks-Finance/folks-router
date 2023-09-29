9f8c30b1be2dac472998bf5b322646b4d4774a57
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2023-09-30 01:31:39 +0200

    fix: move types into related dir, add timeframes mapping

--- apps/website/src/components/metrics-dashboard/types.ts
+++ apps/website/src/components/metrics-dashboard/types.ts
@@ -1,2 +1,9 @@
 export const timeframes = ["YEAR", "MONTH", "DAY", "HOUR"] as const;
 export type Timeframes = (typeof timeframes)[number];
+
+export const TIMEFRAMES_LABEL: { [key: number]: string } = {
+  0: "YEAR",
+  1: "MONTH",
+  2: "DAY",
+  3: "HOUR",
+};
