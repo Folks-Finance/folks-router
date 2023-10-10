add004a85139cebce3ca416b97c0bb5e43d7ead9
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2023-10-10 17:52:34 +0200

    feat: add hook to retrieve folks router assets

---
+++ apps/website/src/lib/metrics/hooks/use-folks-router-assets.ts
@@ -0,0 +1,53 @@
+import { useQuery } from "@tanstack/react-query";
+import { GraphQLClient, gql } from "graphql-request";
+
+import { FOLKS_ROUTER_METRICS_ENDPOINT } from "src/lib/metrics/hooks/graphql-endpoint";
+
+interface BaseAssetInfo {
+  id: string;
+  name: string;
+  ticker: string;
+}
+
+interface AssestInfo {
+  tokens: {
+    totalCount: number;
+    nodes: BaseAssetInfo[];
+  };
+}
+
+const getFolksRouterAssets = async () => {
+  const client = new GraphQLClient(FOLKS_ROUTER_METRICS_ENDPOINT);
+
+  const query = gql`
+    query {
+      tokens(offset: 0) {
+        totalCount
+        nodes {
+          id
+          name
+          ticker
+        }
+      }
+    }
+  `;
+
+  const { tokens } = await client.request<AssestInfo>(query);
+  return { tokens };
+};
+
+export const useFolksRouterAssets = () => {
+  const { data: folksRouterAssets } = useQuery({
+    queryKey: ["folks-router-assets"],
+    queryFn: async () => {
+      const {
+        tokens: { nodes: folksRouterAssets },
+      } = await getFolksRouterAssets();
+      return folksRouterAssets;
+    },
+    staleTime: Infinity,
+    refetchInterval: 30 * 60 * 1000, // 30 minutes
+  });
+
+  return { folksRouterAssets };
+};
