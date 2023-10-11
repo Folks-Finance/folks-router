de8b8e32d60a4adc1d47308e6b2b5bd7e7cae992
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2023-10-11 17:54:57 +0200

    chore: add TokenFeesVolumeChart to dashboard

--- apps/website/src/components/metrics-dashboard/metrics-dashboard-inner.tsx
+++ apps/website/src/components/metrics-dashboard/metrics-dashboard-inner.tsx
@@ -1,5 +1,6 @@
 import { Col, Grid } from "@tremor/react";
 
+import { TokenFeesVolumeChart } from "@components/metrics-dashboard/token-fees-chart/token-fees-volume-chart";
 import { TokenVolumeChart } from "@components/metrics-dashboard/token-volume-chart/token-volume-chart";
 
 export const MetricsDashboardInner = () => {
@@ -7,6 +8,9 @@ export const MetricsDashboardInner = () => {
     <Grid numItems={1} numItemsLg={12} className="gap-6">
       <Col numColSpan={1} numColSpanLg={12}>
         <TokenVolumeChart />
+      </Col>
+      <Col numColSpan={1} numColSpanLg={12}>
+        <TokenFeesVolumeChart />
       </Col>
     </Grid>
   );
