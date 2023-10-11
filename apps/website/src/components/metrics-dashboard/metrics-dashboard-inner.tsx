aa6534e73987a8faacb4c7d7ca70d01a5c6fcca1
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2023-10-11 16:38:33 +0200

    chore: temporarily remove TransactionVolumeChart

--- apps/website/src/components/metrics-dashboard/metrics-dashboard-inner.tsx
+++ apps/website/src/components/metrics-dashboard/metrics-dashboard-inner.tsx
@@ -1,14 +1,10 @@
 import { Col, Grid } from "@tremor/react";
 
 import { TokenVolumeChart } from "@components/metrics-dashboard/token-volume-chart/token-volume-chart";
-import { TransactionVolumeChart } from "@components/metrics-dashboard/transaction-volume-chart/transaction-volume-chart";
 
 export const MetricsDashboardInner = () => {
   return (
     <Grid numItems={1} numItemsLg={12} className="gap-6">
-      <Col numColSpan={1} numColSpanLg={12}>
-        <TransactionVolumeChart />
-      </Col>
       <Col numColSpan={1} numColSpanLg={12}>
         <TokenVolumeChart />
       </Col>
