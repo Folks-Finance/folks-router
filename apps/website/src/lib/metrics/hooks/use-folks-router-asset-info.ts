760db7f5059611886170d69c25ec9a8d530857cc
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2023-10-12 18:21:36 +0200

    fix: add timeframe to trigger query

--- apps/website/src/lib/metrics/hooks/use-folks-router-asset-info.ts
+++ apps/website/src/lib/metrics/hooks/use-folks-router-asset-info.ts
@@ -12,7 +12,7 @@ interface BaseFolksRouterAssetInfo {
 }
 
 interface FolksRouterAssestInfo {
-  tokenHourData: {
+  tokenData: {
     nodes: BaseFolksRouterAssetInfo[];
   };
 }
@@ -20,28 +20,66 @@ interface FolksRouterAssestInfo {
 const getFolksRouterAssetInfo = async (assetId: string, timeframe: Timeframes) => {
   const client = new GraphQLClient(FOLKS_ROUTER_METRICS_ENDPOINT);
 
-  console.log(timeframe);
-
-  const query = gql`
-    query ($assetId: String!) {
-      tokenHourData(last: 168, filter: { tokenId: { equalTo: $assetId } }, orderBy: PERIOD_START_UNIX_ASC) {
-        nodes {
-          periodStartUnix
-          volumeUSD
-          feesUSD
+  let query = "";
+  // A year by MONTH
+  if (timeframe === "MONTH") {
+    query = gql`
+      query ($assetId: String!) {
+        tokenData: tokenMonthData(
+          last: 12
+          filter: { tokenId: { equalTo: $assetId } }
+          orderBy: PERIOD_START_UNIX_ASC
+        ) {
+          nodes {
+            periodStartUnix
+            volumeUSD
+            feesUSD
+          }
         }
       }
-    }
-  `;
+    `;
+  }
+  // A month by DAY
+  if (timeframe === "DAY") {
+    query = gql`
+      query ($assetId: String!) {
+        tokenData: tokenDayData(last: 31, filter: { tokenId: { equalTo: $assetId } }, orderBy: PERIOD_START_UNIX_ASC) {
+          nodes {
+            periodStartUnix
+            volumeUSD
+            feesUSD
+          }
+        }
+      }
+    `;
+  }
+  // A week by HOUR
+  if (timeframe === "HOUR") {
+    query = gql`
+      query ($assetId: String!) {
+        tokenData: tokenHourData(
+          last: 168
+          filter: { tokenId: { equalTo: $assetId } }
+          orderBy: PERIOD_START_UNIX_ASC
+        ) {
+          nodes {
+            periodStartUnix
+            volumeUSD
+            feesUSD
+          }
+        }
+      }
+    `;
+  }
 
-  const { tokenHourData } = await client.request<FolksRouterAssestInfo>(query, { assetId });
-  return { tokenHourData };
+  const { tokenData } = await client.request<FolksRouterAssestInfo>(query, { assetId });
+  return { tokenData };
 };
 
 export const useFolksRouterAssetInfo = ({ assetId, timeframe }: { assetId: string; timeframe: Timeframes }) => {
   const fetchAssetInformation = (assetId: string, timeframe: Timeframes) => {
     const { data, ...rest } = useQuery({
-      queryKey: ["folks-router-asset-info", assetId],
+      queryKey: ["folks-router-asset-info", assetId, timeframe],
       queryFn: () => getFolksRouterAssetInfo(assetId, timeframe),
       staleTime: Infinity,
       refetchInterval: 30 * 60 * 1000, // 30 minutes
