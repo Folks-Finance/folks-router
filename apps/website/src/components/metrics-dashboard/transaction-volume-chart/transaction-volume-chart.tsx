c6b2bb2871c1bf8b24599a3dae5ea3aea4aa39bf
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2023-10-13 11:28:48 +0200

    feat: add chart and logic to TransactionVolumeChart

--- apps/website/src/components/metrics-dashboard/transaction-volume-chart/transaction-volume-chart.tsx
+++ apps/website/src/components/metrics-dashboard/transaction-volume-chart/transaction-volume-chart.tsx
@@ -1,10 +1,46 @@
-import { Card, Tab, TabGroup, TabList } from "@tremor/react";
+import { BarChart, Card, Tab, TabGroup, TabList } from "@tremor/react";
 import { useState } from "react";
+import { useSpinDelay } from "spin-delay";
 
 import { timeframes } from "@components/metrics-dashboard/timeframes";
+import { Spinner } from "@components/spinner/spinner";
+import { useIsMobile } from "@hooks/use-device-size";
+import { useFolksRouterData } from "src/lib/metrics/hooks/use-folks-router-data";
+
+const valueFormatter = (value: number) => `$ ${new Intl.NumberFormat("en-US").format(value).toString()}`;
 
 export const TransactionVolumeChart = () => {
   const [selectedTimeframeIndex, setSelectedTimeframeIndex] = useState(1); // Default to DAY
+  const selectedTimeframe = timeframes[selectedTimeframeIndex] || timeframes[1];
+
+  const isMobile = useIsMobile();
+
+  const { data, isLoading: isFolksRouterDataLoading } = useFolksRouterData({
+    timeframe: selectedTimeframe,
+  });
+
+  const isDataLoading = useSpinDelay(isFolksRouterDataLoading, {
+    delay: 0,
+    minDuration: 1000,
+  });
+
+  const folksRouterData = data?.folksRouterData.nodes;
+  if (!folksRouterData || isDataLoading)
+    return (
+      <Card className="flex min-h-[34.75rem] w-full items-center justify-center sm:min-h-[31.375rem] tablet:min-h-[28.375rem]">
+        <Spinner />
+      </Card>
+    );
+
+  const transactionVolumeChartData = folksRouterData.map(({ periodStartUnix, volumeUSD }) => ({
+    "transaction-volume": new Date(periodStartUnix).toLocaleDateString("en-US", {
+      hour: "2-digit",
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+    }),
+    "Transaction Volume": volumeUSD,
+  }));
 
   return (
     <Card className="flex flex-col gap-y-12">
@@ -32,7 +68,18 @@ export const TransactionVolumeChart = () => {
         </div>
       </div>
 
-      {/* Transaction Volume Chart */}
+      <BarChart
+        data={transactionVolumeChartData}
+        index="transaction-volume"
+        categories={["Transaction Volume"]}
+        colors={["blue"]}
+        valueFormatter={valueFormatter}
+        yAxisWidth={80}
+        showAnimation
+        showXAxis={false}
+        showYAxis={isMobile ? false : true}
+        showLegend={false}
+      />
     </Card>
   );
 };
