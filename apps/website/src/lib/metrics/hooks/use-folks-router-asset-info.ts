47e43ea9a04b00ea5caa440f3ec30793a9059102
Stefano Faieta <9802152+stefanofa@users.noreply.github.com>
2023-10-13 15:53:49 +0200

    refactor: improve use folks router asset info

--- apps/website/src/lib/metrics/hooks/use-folks-router-asset-info.ts
+++ apps/website/src/lib/metrics/hooks/use-folks-router-asset-info.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { GraphQLClient, gql } from "graphql-request";
+import { match } from "ts-pattern";
 
 import { FOLKS_ROUTER_METRICS_ENDPOINT } from "src/lib/metrics/hooks/graphql-endpoint";
 
@@ -20,74 +21,74 @@ interface FolksRouterAssestInfo {
 const getFolksRouterAssetInfo = async (assetId: string, timeframe: Timeframes) => {
   const client = new GraphQLClient(FOLKS_ROUTER_METRICS_ENDPOINT);
 
-  let query = "";
-  // A year by MONTH
-  if (timeframe === "MONTH") {
-    query = gql`
-      query ($assetId: String!) {
-        tokenData: tokenMonthData(
-          last: 12
-          filter: { tokenId: { equalTo: $assetId } }
-          orderBy: PERIOD_START_UNIX_ASC
-        ) {
-          nodes {
-            periodStartUnix
-            volumeUSD
-            feesUSD
+  const query = match(timeframe)
+    .with(
+      "MONTH",
+      () => gql`
+        query ($assetId: String!) {
+          tokenData: tokenMonthData(
+            last: 12
+            filter: { tokenId: { equalTo: $assetId } }
+            orderBy: PERIOD_START_UNIX_ASC
+          ) {
+            nodes {
+              periodStartUnix
+              volumeUSD
+              feesUSD
+            }
           }
         }
-      }
-    `;
-  }
-  // A month by DAY
-  if (timeframe === "DAY") {
-    query = gql`
-      query ($assetId: String!) {
-        tokenData: tokenDayData(last: 31, filter: { tokenId: { equalTo: $assetId } }, orderBy: PERIOD_START_UNIX_ASC) {
-          nodes {
-            periodStartUnix
-            volumeUSD
-            feesUSD
+      `,
+    )
+    .with(
+      "DAY",
+      () => gql`
+        query ($assetId: String!) {
+          tokenData: tokenDayData(
+            last: 31
+            filter: { tokenId: { equalTo: $assetId } }
+            orderBy: PERIOD_START_UNIX_ASC
+          ) {
+            nodes {
+              periodStartUnix
+              volumeUSD
+              feesUSD
+            }
           }
         }
-      }
-    `;
-  }
-  // A week by HOUR
-  if (timeframe === "HOUR") {
-    query = gql`
-      query ($assetId: String!) {
-        tokenData: tokenHourData(
-          last: 168
-          filter: { tokenId: { equalTo: $assetId } }
-          orderBy: PERIOD_START_UNIX_ASC
-        ) {
-          nodes {
-            periodStartUnix
-            volumeUSD
-            feesUSD
+      `,
+    )
+    .with(
+      "HOUR",
+      () => gql`
+        query ($assetId: String!) {
+          tokenData: tokenHourData(
+            last: 168
+            filter: { tokenId: { equalTo: $assetId } }
+            orderBy: PERIOD_START_UNIX_ASC
+          ) {
+            nodes {
+              periodStartUnix
+              volumeUSD
+              feesUSD
+            }
           }
         }
-      }
-    `;
-  }
+      `,
+    )
+    .exhaustive();
 
   const { tokenData } = await client.request<FolksRouterAssestInfo>(query, { assetId });
   return { tokenData };
 };
 
 export const useFolksRouterAssetInfo = ({ assetId, timeframe }: { assetId: string; timeframe: Timeframes }) => {
-  const fetchAssetInformation = (assetId: string, timeframe: Timeframes) => {
-    const { data, ...rest } = useQuery({
-      queryKey: ["folks-router-asset-info", assetId, timeframe],
-      queryFn: () => getFolksRouterAssetInfo(assetId, timeframe),
-      staleTime: Infinity,
-      refetchInterval: 30 * 60 * 1000, // 30 minutes
-    });
+  const { data, ...rest } = useQuery({
+    queryKey: ["folks-router-asset-info", assetId, timeframe],
+    queryFn: () => getFolksRouterAssetInfo(assetId, timeframe),
+    staleTime: Infinity,
+    refetchInterval: 30 * 60 * 1000, // 30 minutes
+  });
 
-    return { data, ...rest };
-  };
-
-  const { data, ...rest } = fetchAssetInformation(assetId, timeframe);
   return { data, ...rest };
 };
