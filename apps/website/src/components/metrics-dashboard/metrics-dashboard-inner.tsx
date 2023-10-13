abb60db6b6bdda433edf86f3bafe1eae951a3c6e
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2023-10-13 11:29:23 +0200

    fix: add TransactionVolumeChart and TransactionFeesVolumeChart to dashboard

--- apps/website/src/components/metrics-dashboard/metrics-dashboard-inner.tsx
+++ apps/website/src/components/metrics-dashboard/metrics-dashboard-inner.tsx
@@ -2,10 +2,18 @@ import { Col, Grid } from "@tremor/react";
 
 import { TokenFeesVolumeChart } from "@components/metrics-dashboard/token-fees-volume-chart/token-fees-volume-chart";
 import { TokenVolumeChart } from "@components/metrics-dashboard/token-volume-chart/token-volume-chart";
+import { TransactionFeesVolumeChart } from "@components/metrics-dashboard/transaction-fees-volume-chart/transaction-fees-volume-chart";
+import { TransactionVolumeChart } from "@components/metrics-dashboard/transaction-volume-chart/transaction-volume-chart";
 
 export const MetricsDashboardInner = () => {
   return (
     <Grid numItems={1} numItemsLg={12} className="gap-6">
+      <Col numColSpan={1} numColSpanLg={12}>
+        <TransactionVolumeChart />
+      </Col>
+      <Col numColSpan={1} numColSpanLg={12}>
+        <TransactionFeesVolumeChart />
+      </Col>
       <Col numColSpan={1} numColSpanLg={12}>
         <TokenVolumeChart />
       </Col>
