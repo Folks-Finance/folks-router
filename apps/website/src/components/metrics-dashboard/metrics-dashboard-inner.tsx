0dfe47fb4279e3ecfd594884cd9c8b2a743934b7
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2023-10-13 13:00:44 +0200

    chore: remove old charts, add new one

--- apps/website/src/components/metrics-dashboard/metrics-dashboard-inner.tsx
+++ apps/website/src/components/metrics-dashboard/metrics-dashboard-inner.tsx
@@ -3,9 +3,8 @@ import { Col, Grid } from "@tremor/react";
 import { TokenFeesVolumeChart } from "@components/metrics-dashboard/token-fees-volume-chart/token-fees-volume-chart";
 import { TokenVolumeChart } from "@components/metrics-dashboard/token-volume-chart/token-volume-chart";
 import { TransactionFeesVolumeChart } from "@components/metrics-dashboard/transaction-fees-volume-chart/transaction-fees-volume-chart";
-import { TransactionNumberChart } from "@components/metrics-dashboard/transaction-number-chart/transaction-number-chart";
 import { TransactionVolumeChart } from "@components/metrics-dashboard/transaction-volume-chart/transaction-volume-chart";
-import { WalletNumberChart } from "@components/metrics-dashboard/wallet-number-chart/wallet-number-chart";
+import { TransactionWalletNumberChart } from "@components/metrics-dashboard/transaction-wallet-chart/transaction-wallet-chart";
 
 export const MetricsDashboardInner = () => {
   return (
@@ -17,10 +16,7 @@ export const MetricsDashboardInner = () => {
         <TransactionFeesVolumeChart />
       </Col>
       <Col numColSpan={1} numColSpanLg={12}>
-        <WalletNumberChart />
-      </Col>
-      <Col numColSpan={1} numColSpanLg={12}>
-        <TransactionNumberChart />
+        <TransactionWalletNumberChart />
       </Col>
       <Col numColSpan={1} numColSpanLg={12}>
         <TokenVolumeChart />
