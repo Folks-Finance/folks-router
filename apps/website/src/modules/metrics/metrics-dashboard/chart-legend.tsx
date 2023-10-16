eefb2a6bab742e69d760e5e49306601f51989fc3
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2023-10-16 11:41:51 +0200

    fix: add flex-wrap

--- apps/website/src/modules/metrics/metrics-dashboard/chart-legend.tsx
+++ apps/website/src/modules/metrics/metrics-dashboard/chart-legend.tsx
@@ -6,7 +6,7 @@ export const ChartLegend = ({ xLabel, yLabel }: { xLabel: string; yLabel: string
   if (!isMobile) return null;
 
   return (
-    <div className="flex w-full items-center justify-center gap-x-4 text-sm text-light-1">
+    <div className="flex w-full flex-wrap items-center justify-center gap-x-4 text-sm text-light-1">
       <span>x: {xLabel}</span>
       <span>y: {yLabel}</span>
     </div>
